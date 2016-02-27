dbd30124c0844dba25c1a1f5304ed91654ebaaf9
cristianlh <vj.clazaro@gmail.com>
2016-02-27 16:56:54 +0100

    Integrando Possession. Añadir codigo pruebas antes de pushear

---
+++ Engine/logic/ai_poss.cpp
@@ -0,0 +1,96 @@
+#include "mcv_platform.h"
+#include "ai_poss.h"
+#include "components\entity.h"
+
+ai_poss::ai_poss() {
+	AddState(ST_POSSESSING, (statehandler)&ai_poss::PossessingState);
+	AddState(ST_POSSESSED, (statehandler)&ai_poss::PossessedState);
+	AddState(ST_UNPOSSESSING, (statehandler)&ai_poss::UnpossessingState);
+	AddState(ST_STUNT, (statehandler)&ai_poss::StuntState);
+	AddState(ST_STUNT_END, (statehandler)&ai_poss::_StuntEndState);
+}
+
+// MENSAJES
+void ai_poss::onSetPossessed(const TMsgAISetPossessed& msg) {
+	dbg("ai_poss, recibe Set Possessed = %d\n", msg.possessed);
+	if (msg.possessed) {
+		_actionBeforePossession();
+		ChangeState(ST_POSSESSING);
+	}
+	else {
+		ChangeState(ST_UNPOSSESSING);
+	}
+}
+
+//ACCIONES implementables
+void ai_poss::_actionBeforePossession() {
+	//Default: Nothing to do
+	dbg("ai_poss::actionBeforePossession\n");
+}
+
+const void ai_poss::PossessingState() {
+	//Default: Nothing to do
+	CEntity* me;
+	switch (_actionBeingPossessed()) {
+	case DONE:
+		dbg("Poseido!\n");
+		ChangeState(ST_POSSESSED);
+		me = getMyEntity();
+		TMsgPossControllerSetEnable msg;
+		msg.enabled = true;
+		me->sendMsg(msg);
+		break;
+
+	case IN_PROGRESS:
+		break;
+
+	case CANCEL:
+		fatal("AI_POSS: Cancel result of actionBeingPossessed is not expected!");
+		break;
+	}
+}
+
+ACTION_RESULT ai_poss::_actionBeingPossessed() {
+	//Ejecutar animacion, etc
+	dbg("Siendo poseido...\n");
+	if (rand() % 10 == 0) return DONE;
+	else return IN_PROGRESS;
+}
+
+const void ai_poss::PossessedState() {
+	// Nothing to do?
+}
+
+ACTION_RESULT ai_poss::_actionBeingUnpossessed() {
+	dbg("Being Dispossessed");
+	//Ejecutar animacion etc.
+	return DONE;
+}
+
+const void ai_poss::UnpossessingState() {
+	switch (_actionBeingUnpossessed()) {
+	case DONE:
+		ChangeState(ST_STUNT);
+		//TODO: Enviar mensaje a la misma entidad para apagar PossController (declarado)
+		break;
+
+	case IN_PROGRESS:
+		break;
+
+	case CANCEL:
+		fatal("AI_POSS: Cancel result of actionBeingDisPossessed is not expected!");
+		break;
+	}
+}
+
+const void ai_poss::StuntState() {
+	actionStunt();
+	____TIMER_CHECK_DO_(timeStunt);
+	ChangeState(ST_STUNT_END);
+	____TIMER_CHECK_DONE_(timeStunt);
+}
+
+// Things to do when is possessed
+void ai_poss::actionStunt() {
+	// Nothing at the moment
+}
