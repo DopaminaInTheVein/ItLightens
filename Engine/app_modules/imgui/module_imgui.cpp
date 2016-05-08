2af7c6583757726c3d0219a4a9f1c9428e145f87
AlexandreAlberti <alberti.bu@gmail.com>
2016-05-08 23:56:05 +0200

    imgui hidded for release

--- Engine/app_modules/imgui/module_imgui.cpp
+++ Engine/app_modules/imgui/module_imgui.cpp
@@ -16,189 +16,188 @@
 #include <Commdlg.h>
 
 bool CImGuiModule::start() {
-  CApp& app = CApp::get();
-  return ImGui_ImplDX11_Init(app.getHWnd(), Render.device, Render.ctx);
+	CApp& app = CApp::get();
+	return ImGui_ImplDX11_Init(app.getHWnd(), Render.device, Render.ctx);
 }
 
 void CImGuiModule::stop() {
-  ImGui_ImplDX11_Shutdown();
+	ImGui_ImplDX11_Shutdown();
 }
 
 void CImGuiModule::update(float dt) {
-  ImGui_ImplDX11_NewFrame();
+	ImGui_ImplDX11_NewFrame();
 
-  ImGuiWindowFlags window_flags = 0;
-  window_flags |= ImGuiWindowFlags_MenuBar;
-  bool menu = true;
+	ImGuiWindowFlags window_flags = 0;
+	window_flags |= ImGuiWindowFlags_MenuBar;
+	bool menu = true;
 #ifndef NDEBUG
-#else
-  ImGui::Begin("Debug UI", &menu, ImVec2(800, 512), -1.0f, window_flags);
-  ImGui::PushItemWidth(-140);                                 // Right align, keep 140 pixels for labels
+	ImGui::Begin("Debug UI", &menu, ImVec2(800, 512), -1.0f, window_flags);
+	ImGui::PushItemWidth(-140);                                 // Right align, keep 140 pixels for labels
 
-  //Console log
-  //---------------------------------------
-  if (ImGui::BeginMenuBar())
-  {
-    if (ImGui::BeginMenu("Console Debug"))
-    {
-      ImGui::MenuItem("Log (L)", NULL, Debug->getStatus());
-      ImGui::MenuItem("Commands (O)", NULL, Debug->GetCommandsConsoleState());
-      //Debug->OpenConsole();
-      ImGui::EndMenu();
-    }
-    ImGui::EndMenuBar();
-  }
-  //---------------------------------------
+	//Console log
+	//---------------------------------------
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("Console Debug"))
+		{
+			ImGui::MenuItem("Log (L)", NULL, Debug->getStatus());
+			ImGui::MenuItem("Commands (O)", NULL, Debug->GetCommandsConsoleState());
+			//Debug->OpenConsole();
+			ImGui::EndMenu();
+		}
+		ImGui::EndMenuBar();
+	}
+	//---------------------------------------
 
-  //Buttons game
-  //---------------------------------------
-  if (GameController->GetGameState() == CGameController::RUNNING) {
-    if (ImGui::Button("PAUSE BUTTON"))
-      GameController->SetGameState(CGameController::STOPPED);
+	//Buttons game
+	//---------------------------------------
+	if (GameController->GetGameState() == CGameController::RUNNING) {
+		if (ImGui::Button("PAUSE BUTTON"))
+			GameController->SetGameState(CGameController::STOPPED);
 
-    ImGui::SameLine();
-    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 1, 0, 1));
-    if (ImGui::Button("RESUME BUTTON"))
-      GameController->SetGameState(CGameController::RUNNING);
+		ImGui::SameLine();
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 1, 0, 1));
+		if (ImGui::Button("RESUME BUTTON"))
+			GameController->SetGameState(CGameController::RUNNING);
 
-    ImGui::PopStyleColor();
-  }
+		ImGui::PopStyleColor();
+	}
 
-  if (GameController->GetGameState() == CGameController::STOPPED) {
-    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 1, 0, 1));
-    if (ImGui::Button("PAUSE BUTTON"))
-      GameController->SetGameState(CGameController::STOPPED);
+	if (GameController->GetGameState() == CGameController::STOPPED) {
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 1, 0, 1));
+		if (ImGui::Button("PAUSE BUTTON"))
+			GameController->SetGameState(CGameController::STOPPED);
 
-    ImGui::PopStyleColor();
+		ImGui::PopStyleColor();
 
-    ImGui::SameLine();
+		ImGui::SameLine();
 
-    if (ImGui::Button("RESUME BUTTON"))
-      GameController->SetGameState(CGameController::RUNNING);
-  }
+		if (ImGui::Button("RESUME BUTTON"))
+			GameController->SetGameState(CGameController::RUNNING);
+	}
 
-  ImGui::Checkbox("Free camera (K)", GameController->GetFreeCameraPointer());
+	ImGui::Checkbox("Free camera (K)", GameController->GetFreeCameraPointer());
 
-  ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 0, 0, 1));
-  ImGui::Text("WARNING: The player will still move, pause the game to stop moving the player");
-  ImGui::PopStyleColor();
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 0, 0, 1));
+	ImGui::Text("WARNING: The player will still move, pause the game to stop moving the player");
+	ImGui::PopStyleColor();
 
-  if (ImGui::TreeNode("Free camera instructions")) {
-    ImGui::Text("w/a/s/d - normal move\nq/e - up/down\nmouse wheel - speed up/down\n");
+	if (ImGui::TreeNode("Free camera instructions")) {
+		ImGui::Text("w/a/s/d - normal move\nq/e - up/down\nmouse wheel - speed up/down\n");
 
-    ImGui::TreePop();
-  }
+		ImGui::TreePop();
+	}
 
-  ImGui::Separator();
-  //---------------------------------------
+	ImGui::Separator();
+	//---------------------------------------
 
-  //Profiling
-  //---------------------------------------
+	//Profiling
+	//---------------------------------------
 #ifdef PROFILING_ENABLED
   //header for filtering instructions
-  if (ImGui::TreeNode("PROFILING"))
-  {
-    static int nframes = 5;
-    ImGui::InputInt("NFrames", &nframes, 1, 5);
-    if (ImGui::Button("Start Capture Profiling"))
-      profiler.setNFramesToCapture(nframes);
-    ImGui::TreePop();
-  }
+	if (ImGui::TreeNode("PROFILING"))
+	{
+		static int nframes = 5;
+		ImGui::InputInt("NFrames", &nframes, 1, 5);
+		if (ImGui::Button("Start Capture Profiling"))
+			profiler.setNFramesToCapture(nframes);
+		ImGui::TreePop();
+	}
 #endif
-  //---------------------------------------
+	//---------------------------------------
 
-  //Filter options
-  //---------------------------------------
-  ImGui::Text("Filtering");
+	//Filter options
+	//---------------------------------------
+	ImGui::Text("Filtering");
 
-  //header for filtering instructions
-  if (ImGui::TreeNode("instructions filtering"))
-  {
-    ImGui::Text("Filter usage:\n"
-      "  \"\"         display all lines\n"
-      "  \"xxx\"      display lines containing \"xxx\"\n"
-      "  \"xxx,yyy\"  display lines containing \"xxx\" or \"yyy\"\n"
-      "  \"-xxx\"     hide lines containing \"xxx\"");
-    ImGui::TreePop();
-  }
-  //end header instructions
+	//header for filtering instructions
+	if (ImGui::TreeNode("instructions filtering"))
+	{
+		ImGui::Text("Filter usage:\n"
+			"  \"\"         display all lines\n"
+			"  \"xxx\"      display lines containing \"xxx\"\n"
+			"  \"xxx,yyy\"  display lines containing \"xxx\" or \"yyy\"\n"
+			"  \"-xxx\"     hide lines containing \"xxx\"");
+		ImGui::TreePop();
+	}
+	//end header instructions
 
-  static ImGuiTextFilter filter;
-  filter.Draw(); //filter text draw
-  //---------------------------------------
+	static ImGuiTextFilter filter;
+	filter.Draw(); //filter text draw
+	//---------------------------------------
 
-  if (ImGui::CollapsingHeader("General Info")) {
-    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-  }
-  if (ImGui::CollapsingHeader("Resources")) {
-    Resources.renderUIDebug(&filter);
-  }
+	if (ImGui::CollapsingHeader("General Info")) {
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	}
+	if (ImGui::CollapsingHeader("Resources")) {
+		Resources.renderUIDebug(&filter);
+	}
 
-  if (ImGui::CollapsingHeader("Entities")) {
-    getHandleManager<CEntity>()->onAll(&CEntity::renderInMenu);
-  }
+	if (ImGui::CollapsingHeader("Entities")) {
+		getHandleManager<CEntity>()->onAll(&CEntity::renderInMenu);
+	}
 
-  if (ImGui::CollapsingHeader("SELECTED ENTITY")) {
-    ImGui::Text("Application SELECTED ENTITY - TODO");
-  }if (ImGui::CollapsingHeader("Entity by Tag")) {
-    ImGui::Text("Application ENTITY TAG - TODO");
-    tags_manager.renderInMenu();
-  }if (ImGui::CollapsingHeader("Graficos")) {
-    if (ImGui::TreeNode("polarize")) {
-      ImGui::SliderFloat("Polarize strength", &shader_ctes_globals.strenght_polarize, 0.0f, 2.0f);
+	if (ImGui::CollapsingHeader("SELECTED ENTITY")) {
+		ImGui::Text("Application SELECTED ENTITY - TODO");
+	}if (ImGui::CollapsingHeader("Entity by Tag")) {
+		ImGui::Text("Application ENTITY TAG - TODO");
+		tags_manager.renderInMenu();
+	}if (ImGui::CollapsingHeader("Graficos")) {
+		if (ImGui::TreeNode("polarize")) {
+			ImGui::SliderFloat("Polarize strength", &shader_ctes_globals.strenght_polarize, 0.0f, 2.0f);
 
-      ImGui::TreePop();
-    }
+			ImGui::TreePop();
+		}
 
-    if (ImGui::TreeNode("Grafic options")) {
-      ImGui::Checkbox("polarize effects", GameController->GetFxPolarizePointer());
+		if (ImGui::TreeNode("Grafic options")) {
+			ImGui::Checkbox("polarize effects", GameController->GetFxPolarizePointer());
 
-      ImGui::Checkbox("glow effect", GameController->GetFxGlowPointer());
-	  
-      ImGui::TreePop();
-    }
-  }if (ImGui::CollapsingHeader("Culling")) {
-	  RenderManager.renderUICulling();
-	  ImGui::Checkbox("show culling collider", GameController->GetCullingRenderPointer());
-  }
+			ImGui::Checkbox("glow effect", GameController->GetFxGlowPointer());
 
-  ImGui::End();
+			ImGui::TreePop();
+		}
+	}if (ImGui::CollapsingHeader("Culling")) {
+		RenderManager.renderUICulling();
+		ImGui::Checkbox("show culling collider", GameController->GetCullingRenderPointer());
+	}
+
+	ImGui::End();
 #endif
-  //TESTS DEBUG:
-  //TestGameLog();
-  //testLines();
-  //bool open = true;
-  //ShowExampleAppConsole(&open);		//test console commands
+	//TESTS DEBUG:
+	//TestGameLog();
+	//testLines();
+	//bool open = true;
+	//ShowExampleAppConsole(&open);		//test console commands
 
-  ui.update();			//update ui
-  //Debug->update();		//update log
+	ui.update();			//update ui
+	//Debug->update();		//update log
 }
 
 void CImGuiModule::render() {
-  activateZ(ZCFG_ALL_DISABLED);
-  ImGui::Render();
+	activateZ(ZCFG_ALL_DISABLED);
+	ImGui::Render();
 }
 
 bool CImGuiModule::onSysMsg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  return ImGui_ImplDX11_WndProcHandler(hWnd, message, wParam, lParam) ? true : false;
+	return ImGui_ImplDX11_WndProcHandler(hWnd, message, wParam, lParam) ? true : false;
 }
 
 //Open file path
 std::string CImGuiModule::getFilePath(char * filter, HWND owner)
 {
-  OPENFILENAME ofn;
-  char fileName[MAX_PATH] = "";
-  ZeroMemory(&ofn, sizeof(ofn));
-  ofn.lStructSize = sizeof(OPENFILENAME);
-  ofn.hwndOwner = owner;
-  ofn.lpstrFilter = filter;
-  ofn.lpstrFile = fileName;
-  ofn.nMaxFile = MAX_PATH;
-  ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
-  ofn.lpstrDefExt = "";
-  std::string fileNameStr;
-  if (GetOpenFileName(&ofn))
-    fileNameStr = fileName;
+	OPENFILENAME ofn;
+	char fileName[MAX_PATH] = "";
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = owner;
+	ofn.lpstrFilter = filter;
+	ofn.lpstrFile = fileName;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+	ofn.lpstrDefExt = "";
+	std::string fileNameStr;
+	if (GetOpenFileName(&ofn))
+		fileNameStr = fileName;
 
-  return fileNameStr;
+	return fileNameStr;
 }
