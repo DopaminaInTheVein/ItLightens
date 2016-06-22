f3ddd6e24568e906e11b058379f4799ad1ec4383
cristianlh <mcv14_alumne@IDEC-UPF.LOCAL>
2016-06-22 15:03:17 +0200

    apply empty impl

--- Engine/skeleton/skeleton_manager.cpp
+++ Engine/skeleton/skeleton_manager.cpp
@@ -2,3 +2,9 @@
 #include "skeleton_manager.h"
 
 CCoreModel::CCoreModel() : CalCoreModel("unnamed"){}
+
+void CCoreModel::TBoneCorrector::apply(CalModel* model, CalVector target, float amount) 
+{
+	//TODO CLH OJO TODO_SKEL
+
+}
