e7bd4d4125520bd5a5ddfd807711cf54413287f6
cristianlh <vj.clazaro@gmail.com>
2016-07-08 22:17:38 +0200

    xml con salas oculta cosas q no debe

--- bin/data/lua_scripts/scenes/ms3c.lua
+++ bin/data/lua_scripts/scenes/ms3c.lua
@@ -14,19 +14,3 @@ function OnActionSci___tElevator( )
 activateElevator()
 end
 
-function OnAction___tCargador( )
-activateCargadorPlayer()
-end
-
-function OnActionSci___tCargador( )
-activateCargadorSci()
-end
-
-function OnAction___tEnchufe( )
-activateEnchufePlayer()
-end
-
-function OnActionSci___tEnchufe( )
-activateEnchufeSci()
-end
-
