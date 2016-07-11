4edf88f4a2be701229841c8a6a1ddb6691b47f6a
cristianlh <vj.clazaro@gmail.com>
2016-07-11 21:39:39 +0200

    fix anim mole putpila

--- Engine/skeleton_controllers/skc_mole.cpp
+++ Engine/skeleton_controllers/skc_mole.cpp
@@ -137,7 +137,6 @@ void SkelControllerMole::myUpdate()
 			grabbedPila.sendMsg(msgAttach);
 		}
 		else if (currentState == AST_PUT_PILA) {
-			setAction("put_pila", "idle");
 			//setAction("grab_pila_2", "idle");
 			currentState = AST_IDLE;
 		}
