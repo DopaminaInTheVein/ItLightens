a192a2b9cb84f37a64e2ffdac6110fbc7ec1ddd7
cristianlh <vj.clazaro@gmail.com>
2016-10-15 18:58:23 +0200

    fix compilacion macro

--- Engine/utils/utils.cpp
+++ Engine/utils/utils.cpp
@@ -77,7 +77,7 @@ float random(float vmin, float vmax) {
 bool isInRoom(CHandle h) {
 	int pjSala = SBB::readSala();
 	GET_COMP(room, h, TCompRoom);
-	DEBUG_COMP(room) {
+	if (DEBUG_COMP(room)) {
 		dbg("");
 	}
 	return ROOM_IS_IN(room, pjSala);
