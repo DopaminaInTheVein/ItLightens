8f041729af1ad6d4d0781805f6656ba3e454bd32
cristianlh <vj.clazaro@gmail.com>
2016-10-28 03:00:42 +0200

    cambio para hacer release

--- bin/data/lua_scripts/events.lua
+++ bin/data/lua_scripts/events.lua
@@ -526,7 +526,7 @@ function SceneLoaded(real_level)
 	if g_loading_screen then
 		--p:exit_game()
 		p:pause_game()
-		p:putTextUi("loading_skip", "::loading_skip", 0.5, 0.1, "#000000FF", 0.4)
+		p:putTextUi("loading_skip", "::loading_skip", 0.4, 0.1, "#000000FF", 0.4)
 		p:wait_action("InitScene(\""..real_level.."\");");
 		--p:exec_command("InitScene()", 5.0)
 	else
